cb5dd90995a406c8d3234254d85df6c3aa06d42c
Pandenimik <pandenimik@mail.ru>
2022-02-23 09:51:37 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return <div>
     <button onClick={() => showMess(1)}>act1</button>
     <button onClick={() => showMess(2)}>act2</button>
-    <button onClick={() => showMess(3)}>act3</button>
+    <button onClick={() => showMess(4)}>act3</button>
   </div>;
 }
 
